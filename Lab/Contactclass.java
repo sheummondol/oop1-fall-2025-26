115eee55921d13fd89d96ebc51a90e553b8dfcc5
sheummondol <sheummondol2022@gmail.com>
2025-12-10 09:24:03 +0600

    Contact Class

---
+++ Lab/Contactclass.java
@@ -0,0 +1,29 @@
+Contact class {
+    String phone;
+    String email;
+    Contact (String phone,String email)
+    {
+        this.phone=phone;
+        this.email=email;
+
+    }
+    Void showinfo()
+    {
+        System.out.println("Phone:" +phone);
+        System.out.println("Email:"+email);
+
+
+    }
+}
+
+Class School {
+    String schoolName;
+    Student[]students;
+    int count ;
+
+    School(String SchoolName, int size )
+    
+
+
+
+}
